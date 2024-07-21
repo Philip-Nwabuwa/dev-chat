19153bec5dde714714c8966798674f6ba318e28f
philipnwabuwa <philipnwabuwa@gmail.com>
2024-07-21 22:51:45 +0000

    Chore: added sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { validateRequest } from "@/auth";
 import SessionProvider from "@/components/providers/SessionProvider";
 import Navbar from "@/components/common/Navbar";
+import MenuBar from "@/components/common/MenuBar";
 
 export default async function MainLayout({
   children,
@@ -15,7 +16,11 @@ export default async function MainLayout({
     <SessionProvider value={session}>
       <div className="mx-auto flex min-h-screen max-w-7xl flex-col">
         <Navbar />
-        <main className="p-5">{children}</main>
+        <div className="flex w-full grow gap-5 p-5">
+          <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
     </SessionProvider>
   );
